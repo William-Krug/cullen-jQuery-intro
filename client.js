eb364ad83398aab9b6437a43af3e851c2fe8f0f0
William Krug <william.p.krug@gmail.com>
2021-01-28 13:48:15 -0600

    added css file

--- client.js
+++ client.js
@@ -4,4 +4,13 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('I have jQuery');
+
+  const h1 = $('h1');
+  console.log('h1:', h1);
+
+  const h2 = $('h2');
+  // make it blue
+  h2.css('background-color', 'blue');
+  // same as ...
+  $('h2').css('background-color', 'green');
 }
